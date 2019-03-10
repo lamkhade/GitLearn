d27d676357f73cb2c0c77404b59f97ef2443cdcc
lamkhade <ravindra.lamkhade@gmail.com>
2019-03-10 19:35:51 +0530

    Changes to fie

--- calc.py
+++ calc.py
@@ -1,11 +1,10 @@
 def add(x,y):
     pass
 def subtract(x,y):
-    return x-y
+    pass
 def multiply(x,y):
     pass
 def divide(x,y):
-    return x/y
-    pass    # Details of Merging of master with calc-devide
+    pass    
 def square(x,y):
     pass
